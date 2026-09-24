5ce2d436f6e993b4a841276d5a81187615bd7aec
kkayto <heloisaauletta280@gmail.com>
2026-09-24 18:59:46 -0300

    Add files via upload

--- home.js
+++ home.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
         data,
         status
     }) {
-return `
+        return `
     <div class="card-atividades">
 
         <div class="card-atividades-title">
@@ -115,3 +115,202 @@ return `
     console.log('HTML gerado:', container.innerHTML);
 
 });
+
+    /* CALENDÁRIO */
+const monthYear = document.getElementById("mes");
+const daysContainer = document.getElementById("calendarDays");
+const eventInfo = document.getElementById("eventoInfo");
+
+
+const eventos = {
+    "2026-04-24": {
+        tipo: "prova",
+        titulo: "Provão Paulista"
+    },
+
+    "2026-04-28": {
+        tipo: "olimpiada",
+        titulo: "OBMEP"
+    },
+
+    "2026-06-03": {
+        tipo: "prova",
+        titulo: "Prova"
+    },
+
+    "2026-06-08": {
+        tipo: "prova",
+        titulo: "Prova"
+    },
+
+    "2026-06-12": {
+        tipo: "festa",
+        titulo: "Festa Junina"
+    },
+
+    "2026-06-22": {
+        tipo: "festa",
+        titulo: "Festa"
+    },
+
+    "2026-07-06": {
+        tipo: "ferias",
+        titulo: "Início das Férias"
+    }
+};
+
+
+let currentDate = new Date();
+
+
+function renderCalendar() {
+
+    daysContainer.innerHTML = "";
+
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+
+    const firstDay = new Date(year, month, 1).getDay();
+    const totalDays = new Date(year, month + 1, 0).getDate();
+
+    const months = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro"
+    ];
+    monthYear.textContent = `${months[month]} ${year}`;
+
+
+    for (let i = 0; i < firstDay; i++) {
+        const emptyDay = document.createElement("div");
+        daysContainer.appendChild(emptyDay);
+    }
+
+
+    for (let day = 1; day <= totalDays; day++) {
+        const fullDate =
+            `${year}-${String(month + 1).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+        const div = document.createElement("div");
+
+        div.classList.add("day");
+        div.textContent = day;
+
+        if (eventos[fullDate]) {
+            div.classList.add(eventos[fullDate].tipo);
+        }
+
+        const hoje = new Date();
+
+        if (
+            day === hoje.getDate() &&
+            month === hoje.getMonth() &&
+            year === hoje.getFullYear()
+        ) {
+            div.classList.add("today");
+        }
+
+        div.addEventListener("click", () => {
+
+            if (eventos[fullDate]) {
+                eventInfo.innerHTML = `
+                    <strong>${eventos[fullDate].titulo}</strong>
+                    <br>
+                    ${fullDate}
+                `;
+            } else {
+                eventInfo.innerHTML = `
+                    Nenhum evento nesta data
+                `;
+            }
+        });
+
+        daysContainer.appendChild(div);
+    }
+}
+
+renderCalendar();
+
+document.getElementById("next").addEventListener("click", () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    renderCalendar();
+});
+
+
+document.getElementById("prev").addEventListener("click", () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    renderCalendar();
+});
+
+
+const proximos = [
+    {
+        materia: "Prova",
+        titulo: "OBMEP",
+        data: "03/06/2026"
+    },
+
+    {
+        materia: "Festa",
+        titulo: "Festa Junina",
+        data: "12/06/2026",
+        status: "Concluída"
+    },
+
+    {
+        materia: "Prova",
+        titulo: "Prova",
+        data: "08/06/2026"
+    },
+
+    {
+        materia: "Festa",
+        titulo: "Festa",
+        data: "22/06/2026",
+        status: "Concluída"
+    }
+];
+
+
+const proximosContainer =
+    document.getElementById("proximos-container");
+
+if (!proximosContainer) {
+    console.error(
+        "Elemento #proximos-container não encontrado"
+    );
+} else {
+    proximosContainer.innerHTML = proximos
+        .map(criarCardAtividade)
+        .join("");
+}
+
+
+function criarCardAtividade({
+    materia,
+    titulo,
+    data,
+    status
+}) {
+    return `
+        <div class="card-proximos">
+
+            <div class="card-proximos-title">
+                ${materia} - ${titulo}
+            </div>
+
+            <div class="card-proximos-data">
+                ${data}
+            </div>
+
+        </div>
+    `;
+}
